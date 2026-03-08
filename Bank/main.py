b1ba089fd4e4116524dc39bb69a9326ac3c9eea3
cassio_______________ <antoniocassio000@gmail.com>
2026-03-07 22:15:45 -0300

    commit

--- Bank/main.py
+++ Bank/main.py
@@ -2,22 +2,22 @@ from accounts import *
 from bank import *
 from people import *
 
-b = CheckingAccount(2,123,0,123)
-c = Client(18, "luiz", b)
+conta1 = CheckingAccount(2,123,0,123)
+cliente1 = Client(18, "luiz", conta1)
 
-x = People(12,"cassio")
-z = SavingsAcount(1,2)
-y = Client(12, "cassio", z)
-v = Bank()
-v.agencies.extend([1,2])
-v.clients.extend([y,c])
-v.accounts.extend([z,b])
+pessoa2 = People(12,"cassio")
+conta2 = SavingsAcount(1,2)
+cliente2 = Client(12, "cassio", conta2)
 
-if v.authenticate(y, z):
-        z.deposit(12635)
-        z.withdraw(1231)
+banco_1 = Bank()
+banco_1.agencies.extend([1,2])
+banco_1.clients.extend([cliente1,cliente2])
+banco_1.accounts.extend([conta1,conta2])
 
-if v.authenticate(c,b):
+if banco_1.authenticate(cliente1,conta1):
         b.deposit(123)
         b.withdraw(123)
-       
+
+if banco_1.authenticate(cliente2, conta2):
+        z.deposit(12635)
+        z.withdraw(1231)
